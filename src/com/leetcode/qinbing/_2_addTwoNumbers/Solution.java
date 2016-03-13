eda066c47b95eea90312166485350dc6dd043951
xiamingbo <xiamingbo@gmail.com>
2016-03-13 23:01:59 +0800

    problem 2, second uncomplete solution.

--- src/com/leetcode/qinbing/_2_addTwoNumbers/Solution.java
+++ src/com/leetcode/qinbing/_2_addTwoNumbers/Solution.java
@@ -1,75 +1,50 @@
 package com.leetcode.qinbing._2_addTwoNumbers;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * Created by Administrator on 16-3-11.
  */
 public class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int[] resultArray = calTwoNode(l1.val, l2.val);
 
-        ListNode returnListNode;
-        List<Integer> linkedList1 = new LinkedList<Integer>();
-        linkedList1 = listNode2LinkedList(l1, linkedList1);
-        int number1 = linkedList2Number(linkedList1);
-
-        List<Integer> linkedList2 = new LinkedList<Integer>();
-        linkedList2 = listNode2LinkedList(l2, linkedList2);
-        int number2 = linkedList2Number(linkedList2);
-
-        int returnNumber = number1 + number2;
-
-        List<Integer> returnList = number2LinkedList(returnNumber);
-        returnListNode = linkedList2ListNode(returnList);
-
-        return returnListNode;
-    }
-
-    public List<Integer> listNode2LinkedList(ListNode listNode, List<Integer> returnList) {
-        returnList.add(listNode.val);
-        if (listNode.next != null) {
-            listNode2LinkedList(listNode.next, returnList);
-        }
-        return returnList;
-    }
-
-    public int linkedList2Number(List<Integer> linkedList) {
-        int length = linkedList.size();
-        int number = 0;
-        for (int i = 0; i < length; i++) {
-            number += linkedList.get(i) * Math.pow(10, i);
-        }
-        return number;
-    }
-
-
-    public ListNode linkedList2ListNode(List<Integer> linkedList) {
-
-        ListNode listNode = new ListNode(linkedList.get(0));
-
-        if (linkedList.size() > 1) {
-            for (int i = 1; i < linkedList.size() - 1; i++) {
-                ListNode listNode1 = new ListNode(linkedList.get(i));
-                ListNode listNode2 = new ListNode(linkedList.get(i + 1));
-                listNode1.next = listNode2;
-                if (i == 1) {
-                    listNode.next = listNode1;
-                }
-            }
+        ListNode listNode = new ListNode(resultArray[0]);
+        if (resultArray[1] != 0) {
+            ListNode listNode2 = new ListNode(1);
+            listNode.next = listNode2;
         }
         return listNode;
     }
 
-    public List<Integer> number2LinkedList(int number) {
-        String numberString = String.valueOf(number);
-        List<Integer> linkedList = new LinkedList<Integer>();
-        char[] charArray = numberString.toCharArray();
-        for (int i = charArray.length - 1; i >= 0; i--) {
-            int num = Integer.parseInt(String.valueOf(charArray[i]));
-            linkedList.add(num);
+    public int addTwoNodes(ListNode resultListNode, int increaseDigit, ListNode listNode1, ListNode listNode2) {
+        while (listNode1 == null && listNode2 == null) {
+            int listNode1Val = 0;
+            int listNode2Val = 0;
+            if (listNode1 != null) {
+                listNode1Val = listNode1.val;
+            }
+            if (listNode2 != null) {
+                listNode2Val = listNode2.val;
+            }
+            int[] resultArray = calTwoNode(listNode1Val, listNode2Val);
+            ListNode currentNode = new ListNode(resultArray[0]);
+            if (resultArray[1] != 0) {
+                int tempIncreateDigit = addTwoNodes(resultListNode, resultArray[1], listNode1.next, listNode2.next);
+            }
         }
-        return linkedList;
+        return resultListNode;
+    }
+
+    public int[] calTwoNode(int nodeVal1, int nodeVal2) {
+        int[] returnArray = new int[2];
+        int result = nodeVal1 + nodeVal2;
+        if (result < 10) {
+            returnArray[0] = result;
+            returnArray[1] = 0;
+        } else if (result >= 10) {
+            returnArray[0] = result % 10;
+            returnArray[1] = 1;
+        }
+        return returnArray;
     }
 }
--- src/com/leetcode/qinbing/_2_addTwoNumbers/TestAddTwoNumbers.java
+++ src/com/leetcode/qinbing/_2_addTwoNumbers/TestAddTwoNumbers.java
@@ -2,37 +2,49 @@ package com.leetcode.qinbing._2_addTwoNumbers;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 16-3-11.
  */
 public class TestAddTwoNumbers {
-    @Test
-    public void test_can_add_two_listNodes() {
-        ListNode listNode1 = new ListNode(9);
 
-        ListNode listNode20 = new ListNode(1);
-        ListNode listnode21 = new ListNode(9);
-        ListNode listnode22 = new ListNode(9);
-        ListNode listnode23 = new ListNode(9);
-        ListNode listnode24 = new ListNode(9);
-        ListNode listnode25 = new ListNode(9);
-        ListNode listnode26 = new ListNode(9);
-        ListNode listnode27 = new ListNode(9);
-        ListNode listnode28 = new ListNode(9);
-        ListNode listnode29 = new ListNode(9);
-        listnode28.next = listnode29;
-        listnode27.next = listnode28;
-        listnode26.next = listnode27;
-        listnode25.next = listnode26;
-        listnode24.next = listnode25;
-        listnode23.next = listnode24;
-        listnode22.next = listnode23;
-        listnode21.next = listnode22;
-        listNode20.next = listnode21;
+    @Test
+    public void test_can_add_two_single_list_node_to_one_listNode() {
+
+        ListNode listNode1 = new ListNode(9);
+        ListNode listNode2 = new ListNode(9);
+        Solution solution = new Solution();
+        ListNode listNode = solution.addTwoNumbers(listNode1, listNode2);
+        assertEquals(8, listNode.val);
+        assertEquals(1, listNode.next.val);
+    }
+
+    @Test
+    public void test_can_add_two_list_with_multi_nodes_to_one_listNode() {
+        ListNode listNode11 = new ListNode(2);
+        ListNode listNode12 = new ListNode(3);
+        ListNode listNode13 = new ListNode(4);
+        ListNode listNode21 = new ListNode(4);
+        ListNode listNode22 = new ListNode(5);
+        ListNode listNode23 = new ListNode(6);
+
+        listNode22.next = listNode23;
+        listNode21.next = listNode22;
+
+        listNode12.next = listNode13;
+        listNode11.next = listNode12;
 
         Solution solution = new Solution();
-        solution.addTwoNumbers(listNode1, listNode20);
+        ListNode listNode = solution.addTwoNumbers(listNode11, listNode21);
+        assertEquals(6, listNode.val);
+        assertEquals(8, listNode.next.val);
+        assertEquals(1, listNode.next.next.val);
+        assertEquals(0, listNode.next.next.next.val);
+    }
 
-
+    @Test
+    public void test_can_add_two_list_with_unequal_nodes_to_one_listNode() {
+        
     }
 }
